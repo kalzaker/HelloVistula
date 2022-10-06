69232838c5b1b8a00176bf2f908ff13c9f988ce4
kalzaker <115027004+kalzaker@users.noreply.github.com>
2022-10-06 23:10:27 +0300

    Hello Vistula

---
+++ src/pl/vistula/helloworldlutovich/HelloWorldLutovich.java
@@ -0,0 +1,9 @@
+package pl.vistula.helloworldlutovich;
+
+public class HelloWorldLutovich
+{
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Lutovich");
+
+    }
+}
